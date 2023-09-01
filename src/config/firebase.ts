b3a627f8604e4daf666da927a7bc79b9b2d202b2
LavrovNick <lavrovmykola@gmail.com>
2023-09-01 05:59:05 +0300

    add .env

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,19 +1,17 @@
 import {initializeApp} from 'firebase/app';
 import {getDatabase} from 'firebase/database';
+import Config from 'react-native-config';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAK0ESZ1QSoJCmpiELmt-ivTNEiYENMuhM',
-  authDomain: 'todotobuy-41a4d.firebaseapp.com',
-  projectId: 'todotobuy-41a4d',
-  storageBucket: 'todotobuy-41a4d.appspot.com',
-  messagingSenderId: '91544492510',
-  appId: '1:91544492510:web:bb52caf6686f253f2be54f',
+  apiKey: Config.FIREBASE_API_KEY,
+  authDomain: Config.FIREBASE_AUTH_DOMAIN,
+  projectId: Config.FIREBASE_PROJECT_ID,
+  storageBucket: Config.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: Config.FIREBASE_MESSAGING_SENDER_ID,
+  appId: Config.FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 
-export const db = getDatabase(
-  app,
-  'https://todotobuy-41a4d-default-rtdb.europe-west1.firebasedatabase.app/',
-);
+export const db = getDatabase(app, Config.FIREBASE_DATABASE_URL);
 export default app;
